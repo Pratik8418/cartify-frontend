476211953b5ce91a8bcbc0f053813b6a217613ba
Pratik Goti <88868354+Pratik8418@users.noreply.github.com>
2023-06-18 00:46:32 +0530

    working on Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,190 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Container from "../components/Container";
+import mainBanner1 from "../images/main-banner-1.jpg"
+import catBanner1 from "../images/catbanner-01.jpg"
+import catBanner2 from "../images/catbanner-02.jpg"
+import catBanner3 from "../images/catbanner-03.jpg"
+import catBanner4 from "../images/catbanner-04.jpg"
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Container class1="home-wrapper-1 py-5">
+        <div className="row">
+          <div className="col-6">
+            <div className="main-banner position-relative ">
+              <img
+                src={mainBanner1}
+                className="img-fluid rounded-3"
+                alt="main banner"
+              />
+              <div className="main-banner-content position-absolute">
+                <h4>SUPERCHARGED FOR PROS.</h4>
+                <h5>iPad S13+ Pro.</h5>
+                <p>From $999.00 or $41.62/mo.</p>
+                <Link className="button">BUY NOW</Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
+              <div className="small-banner position-relative">
+                <img
+                  src={catBanner1}
+                  className="img-fluid rounded-3"
+                  alt="main banner"
+                />
+                <div className="small-banner-content position-absolute">
+                  <h4>Best Sake</h4>
+                  <h5>iPad S13+ Pro.</h5>
+                  <p>
+                    From $999.00 <br /> or $41.62/mo.
+                  </p>
+                </div>
+              </div>
+              <div className="small-banner position-relative">
+                <img
+                  src={catBanner2}
+                  className="img-fluid rounded-3"
+                  alt="main banner"
+                />
+                <div className="small-banner-content position-absolute">
+                  <h4>NEW ARRIVAL</h4>
+                  <h5>But IPad Air</h5>
+                  <p>
+                    From $999.00 <br /> or $41.62/mo.
+                  </p>
+                </div>
+              </div>
+              <div className="small-banner position-relative ">
+                <img
+                  src={catBanner3}
+                  className="img-fluid rounded-3"
+                  alt="main banner"
+                />
+                <div className="small-banner-content position-absolute">
+                  <h4>NEW ARRIVAL</h4>
+                  <h5>But IPad Air</h5>
+                  <p>
+                    From $999.00 <br /> or $41.62/mo.
+                  </p>
+                </div>
+              </div>
+              <div className="small-banner position-relative ">
+                <img
+                  src={catBanner4}
+                  className="img-fluid rounded-3"
+                  alt="main banner"
+                />
+                <div className="small-banner-content position-absolute">
+                  <h4>NEW ARRIVAL</h4>
+                  <h5>But IPad Air</h5>
+                  <p>
+                    From $999.00 <br /> or $41.62/mo.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+      
+      <Container class1="home-wrapper-2 py-5">
+        <div className="row">
+          <div className="col-12">
+            <div className="servies d-flex align-items-center justify-content-between">
+            {/* {services?.map((i, j) => {
+                return (
+                  <div className="d-flex align-items-center gap-15" key={j}>
+                    <img src={i.image} alt="services" />
+                    <div>
+                      <h6>{i.title}</h6>
+                      <p className="mb-0">{i.tagline}</p>
+                    </div>
+                  </div>
+                );
+              })} */}
+            </div>
+          </div>
+        </div>
+      </Container>
+      <Container class1="home-wrapper-2 py-5">
+        <div className="row">
+          <div className="col-12">
+            <div className="categories d-flex justify-content-between flex-wrap align-items-center">
+              <div className="d-flex gap align-items-center">
+                <div>
+                  <h6>Music & Gaming</h6>
+                  <p>10 Items</p>
+                </div>
+                <img src="images/camera.jpg" alt="camera" />
+              </div>
+              <div className="d-flex gap align-items-center">
+                <div>
+                  <h6>Cameras</h6>
+                  <p>10 Items</p>
+                </div>
+                <img src="images/camera.jpg" alt="camera" />
+              </div>
+              <div className="d-flex gap align-items-center">
+                <div>
+                  <h6>Smart Tv</h6>
+                  <p>10 Items</p>
+                </div>
+                <img src="images/tv.jpg" alt="camera" />
+              </div>
+              <div className="d-flex gap align-items-center">
+                <div>
+                  <h6>Smart Watches</h6>
+                  <p>10 Items</p>
+                </div>
+                <img src="images/headphone.jpg" alt="camera" />
+              </div>
+              <div className="d-flex gap align-items-center">
+                <div>
+                  <h6>Music & Gaming</h6>
+                  <p>10 Items</p>
+                </div>
+                <img src="images/camera.jpg" alt="camera" />
+              </div>
+              <div className="d-flex gap align-items-center">
+                <div>
+                  <h6>Cameras</h6>
+                  <p>10 Items</p>
+                </div>
+                <img src="images/camera.jpg" alt="camera" />
+              </div>
+              <div className="d-flex gap align-items-center">
+                <div>
+                  <h6>Smart Tv</h6>
+                  <p>10 Items</p>
+                </div>
+                <img src="images/tv.jpg" alt="camera" />
+              </div>
+              <div className="d-flex gap align-items-center">
+                <div>
+                  <h6>Smart Watches</h6>
+                  <p>10 Items</p>
+                </div>
+                <img src="images/headphone.jpg" alt="camera" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+     
+      
 
-export default Home
+     
+
+      
+      
+      
+
+    
+    </>
+  );
+};
+
+export default Home;
